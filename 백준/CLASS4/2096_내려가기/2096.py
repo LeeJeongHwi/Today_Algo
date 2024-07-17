03aa3354b9af13e62b8d3bed83b2a94431d48bce
LeeJeongHwi <wjdgnl97@gmail.com>
2024-07-17 16:01:45 +0900

    Update 2096.py

--- 백준/CLASS4/2096_내려가기/2096.py
+++ 백준/CLASS4/2096_내려가기/2096.py
@@ -2,18 +2,20 @@ from sys import stdin
 stdin = open('input.txt')
 n = int(stdin.readline())
 
-maps = [list(map(int,stdin.readline().split())) for _ in range(n)]
 min_visit = [[0]*3 for _ in range(2)]
 max_visit = [[0]*3 for _ in range(2)]
 
+# Maps를 설정해서도 안되고, 2단배열로 설정하는것도 안된다..?
+
 def get_minmax():
     for i in range(1,n+1):
-        max_visit[i%2][0] = max(max_visit[not i%2][0] + maps[i-1][0], max_visit[not i%2][1] + maps[i-1][0])
-        max_visit[i%2][1] = max(max_visit[not i%2][0] + maps[i-1][1], max_visit[not i%2][1] + maps[i-1][1], max_visit[not i%2][2] + maps[i-1][1])
-        max_visit[i%2][2] = max(max_visit[not i%2][1] + maps[i-1][2], max_visit[not i%2][2] + maps[i-1][2])
-        min_visit[i%2][0] = min(min_visit[not i%2][0] + maps[i-1][0], min_visit[not i%2][1] + maps[i-1][0])
-        min_visit[i%2][1] = min(min_visit[not i%2][0] + maps[i-1][1], min_visit[not i%2][1] + maps[i-1][1], min_visit[not i%2][2] + maps[i-1][1])
-        min_visit[i%2][2] = min(min_visit[not i%2][1] + maps[i-1][2], min_visit[not i%2][2] + maps[i-1][2])
+        map1, map2, map3 = map(int,stdin.readline().split())
+        max_visit[i%2][0] = max(max_visit[not i%2][0] + map1, max_visit[not i%2][1] + map1)
+        max_visit[i%2][1] = max(max_visit[not i%2][0] + map2, max_visit[not i%2][1] + map2, max_visit[not i%2][2] + map2)
+        max_visit[i%2][2] = max(max_visit[not i%2][1] + map3, max_visit[not i%2][2] + map3)
+        min_visit[i%2][0] = min(min_visit[not i%2][0] + map1, min_visit[not i%2][1] + map1)
+        min_visit[i%2][1] = min(min_visit[not i%2][0] + map2, min_visit[not i%2][1] + map2, min_visit[not i%2][2] + map2)
+        min_visit[i%2][2] = min(min_visit[not i%2][1] + map3, min_visit[not i%2][2] + map3)
     
     print(max(max_visit[i%2]), min(min_visit[i%2]))
 

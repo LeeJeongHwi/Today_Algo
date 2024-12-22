a3acc3b1e5119fdfda3728f3196d1a43fe8f6ec1
LeeJeongHwi <wjdgnl97@gmail.com>
2024-12-22 17:20:57 +0900

    boj: 1343_폴리오미노

--- 백준/그리디/1343_폴리오미노/1343.py
+++ 백준/그리디/1343_폴리오미노/1343.py
@@ -1,9 +1,26 @@
 from sys import stdin
 stdin = open('input.txt')
 
-board = stdin.readline().split(".")
+inputs = stdin.readline()
 
-def solution(board):
-    
-    for b in board:
-        
+def solution(inputs):
+    boards = inputs.split(".")
+    answer = []
+    for board in boards:
+        chA = board.replace("XXXX","AAAA")
+        chAB = chA.replace("XX","BB")
+        if "X" in chAB:
+            chB = board.replace("XX","BB")
+            if "X" in chB:
+                return -1
+            else:
+                answer.append(chB)
+        else:
+            answer.append(chAB)
+    return ".".join(answer)
+
+print(solution(inputs))
+
+print(solution("XX.XX"))
+print(solution("XX.XXXXXX....XX"))
+print(solution("XX.XXXXXXXXXX..XXXXXXXX...XXXXXX"))
